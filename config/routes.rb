aee34b5c176b51a4fb186540ac528c3e33461b7a
liam027 <liam027@gmail.com>
2020-10-09 08:52:08 -0400

    Add route for redirect info page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'redirects#new'
   get '/:id', to: 'redirects#show'
+  get '/:id/info', to: 'redirects#info'
   resources :redirects, only: [:show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
