27335c760fee682086c69ff929448e96fd958ad5
liam027 <liam027@gmail.com>
2020-10-09 09:08:09 -0400

    Make the tokens shorter

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -2,12 +2,11 @@ class Redirect < ApplicationRecord
   has_many :visits
   before_create :generate_token
 
-
   def generate_token
     # Generate a random token to use for the shortened URL. Verify it doesn't
     # already exist and if so generate a new token
     self.token = loop do
-      token = SecureRandom.urlsafe_base64(nil, false)
+      token = SecureRandom.urlsafe_base64(nil, false)[0..8]
       break token unless Redirect.exists?(token: token)
     end
   end
