bcb566adf29c067eb5064ca3fef8089f1bbc0017
liam027 <liam027@gmail.com>
2020-10-09 08:15:28 -0400

    Add method for generating a token
    The token will be used as the short version of the URL.

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,3 +1,15 @@
 class Redirect < ApplicationRecord
   has_many :visits
+  before_create :generate_token
+
+
+  def generate_token
+    # Generate a random token to use for the shortened URL. Verify it doesn't
+    # already exist and if so generate a new token
+    self.token = loop do
+      token = SecureRandom.urlsafe_base64(nil, false)
+      break token unless Redirect.exists?(token: token)
+    end
+  end
+
 end
