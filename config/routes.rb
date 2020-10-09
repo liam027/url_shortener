61fcafa307c1230196870321f0cf349a1027b636
liam027 <liam027@gmail.com>
2020-10-09 08:18:09 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/', to: 'redirects#new'
+  get '/:id', to: 'redirects#show'
+  resources :redirects, only: [:show, :new, :create]
+  resources :visits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
