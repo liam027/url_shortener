994819caeb17912a9bcfd0703848b0b41ce79c8d
liam027 <liam027@gmail.com>
2020-10-09 08:51:58 -0400

    Remove visits routes
    We do not need these routes, visits information will be displayed on
    redirect info page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get '/', to: 'redirects#new'
   get '/:id', to: 'redirects#show'
   resources :redirects, only: [:show, :new, :create]
-  resources :visits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
